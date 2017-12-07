9c35b52c100dd5cda4d4a802667f3c504c3d4594
Ben Hanson <benhanson2@icqmail.com>
2017-12-07 21:33:09 +0000

    Added default constructor to memory_file.

--- lexertl/memory_file.hpp
+++ lexertl/memory_file.hpp
@@ -27,14 +27,22 @@ template<typename char_type>
 class basic_memory_file
 {
 public:
+    basic_memory_file() :
+        _data(nullptr),
+        _size(0),
+        _fh(0),
+#ifdef _WIN32
+        _fmh(0)
+#endif
+    {
+    }
+
     basic_memory_file(const char *pathname_) :
         _data(nullptr),
         _size(0),
-#ifdef _WIN32
         _fh(0),
+#ifdef _WIN32
         _fmh(0)
-#else
-        _fh(0)
 #endif
     {
         open(pathname_);
