69d45a46b69792e11b27b8e2de5b5d40b0e02808
Demian M. Nave <dnave@propulsor.net>
2022-09-28 22:15:46 -0400

    Fix exports and installation

--- lexertlConfig.cmake
+++ lexertlConfig.cmake
@@ -1,2 +1,8 @@
+cmake_minimum_required(VERSION @CMAKE_MAJOR_VERSION@.@CMAKE_MINOR_VERSION@)
 include(CMakeFindDependencyMacro)
+set(lexertl_VERSION @lexertl_VERSION@)
+
+@PACKAGE_INIT@
+
 include("${CMAKE_CURRENT_LIST_DIR}/lexertlTargets.cmake")
+check_required_components(lexertl)
