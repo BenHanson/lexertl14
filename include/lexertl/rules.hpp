4c8b1f1afd4d2b6f0132a610788b4e3695c881e9
BenHanson <jamin.hanson@gmail.com>
2024-01-26 13:30:11 +0000

    Removed std:: from runtime_error

--- include/lexertl/rules.hpp
+++ include/lexertl/rules.hpp
@@ -861,7 +861,7 @@ namespace lexertl
                     std::ostringstream ss_;
 
                     ss_ << "Exit state is missing '>' for rule id " << id_ << '.';
-                    throw std::runtime_error(ss_.str());
+                    throw runtime_error(ss_.str());
                 }
 
                 if (push_dfa_)
