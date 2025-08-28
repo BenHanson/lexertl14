c8c6f64cc242e860c55cf708029fa68c5623ed31
BenHanson <jamin.hanson@gmail.com>
2025-08-28 19:01:19 +0100

    Fix to remove_sequence()

--- include/lexertl/abstemious.hpp
+++ include/lexertl/abstemious.hpp
@@ -6,7 +6,10 @@
 #ifndef LEXERTL_ABSTEMIOUS_HPP
 #define LEXERTL_ABSTEMIOUS_HPP
 
+#include "parser/tokeniser/re_token.hpp"
 #include "parser/tokeniser/re_tokeniser.hpp"
+
+#include <vector>
 
 namespace lexertl
 {
@@ -23,7 +26,7 @@ namespace lexertl
 			while (!indexes_.empty())
 			{
 				auto start_ = indexes_.back();
-				const auto idx_ = start_;
+				auto idx_ = start_;
 
 				if (is_end(tokens_, idx_))
 				{
@@ -31,15 +34,15 @@ namespace lexertl
 
 					switch (op_._type)
 					{
-					case lexertl::detail::token_type::AOPT:
-					case lexertl::detail::token_type::AZEROORMORE:
+					case detail::token_type::AOPT:
+					case detail::token_type::AZEROORMORE:
 						remove_sequence(tokens_, start_, idx_);
 						break;
-					case lexertl::detail::token_type::AONEORMORE:
+					case detail::token_type::AONEORMORE:
 						tokens_.erase(tokens_.begin() + idx_);
 						break;
-					case lexertl::detail::token_type::AREPEATN:
-						op_._type = lexertl::detail::token_type::REPEATN;
+					case detail::token_type::AREPEATN:
+						op_._type = detail::token_type::REPEATN;
 						op_._extra = op_._extra.substr(0, op_._extra.find(','));
 						break;
 					default:
@@ -72,13 +75,13 @@ namespace lexertl
 
 				switch (token_._type)
 				{
-				case lexertl::detail::token_type::OR:
+				case detail::token_type::OR:
 					idx_ = end_block(tokens_, idx_ + 1);
 					break;
-				case lexertl::detail::token_type::CLOSEPAREN:
+				case detail::token_type::CLOSEPAREN:
 					++idx_;
 					break;
-				case lexertl::detail::token_type::END:
+				case detail::token_type::END:
 					return true;
 				default:
 					return false;
@@ -100,21 +103,21 @@ namespace lexertl
 
 				switch (token_._type)
 				{
-				case lexertl::detail::token_type::OR:
+				case detail::token_type::OR:
 					if (parens_ == 0)
 						return idx_;
 
 					break;
-				case lexertl::detail::token_type::OPENPAREN:
+				case detail::token_type::OPENPAREN:
 					++parens_;
 					break;
-				case lexertl::detail::token_type::CLOSEPAREN:
+				case detail::token_type::CLOSEPAREN:
 					if (parens_ == 0)
 						return idx_;
 
 					--parens_;
 					break;
-				case lexertl::detail::token_type::END:
+				case detail::token_type::END:
 					return idx_;
 				default:
 					break;
@@ -125,12 +128,12 @@ namespace lexertl
 		}
 
 		static void remove_sequence(token_vector& tokens_, std::size_t& start_,
-			const std::size_t idx_)
+			std::size_t& idx_)
 		{
 			auto iter_ = tokens_.begin() + idx_ - 1;
 
 			if (iter_->_type ==
-				lexertl::detail::token_type::CLOSEPAREN)
+				detail::token_type::CLOSEPAREN)
 			{
 				// Find beginning of block
 				std::size_t parens_ = 1;
@@ -141,10 +144,10 @@ namespace lexertl
 
 					switch (iter_->_type)
 					{
-					case lexertl::detail::token_type::OPENPAREN:
+					case detail::token_type::OPENPAREN:
 						--parens_;
 						break;
-					case lexertl::detail::token_type::CLOSEPAREN:
+					case detail::token_type::CLOSEPAREN:
 						++parens_;
 						break;
 					default:
@@ -154,12 +157,27 @@ namespace lexertl
 			}
 
 			start_ = iter_ - tokens_.begin();
+
+			// This simplistic approach works, because we know there can't be
+			// a trailing greedy operator following a close paren as we
+			// wouldn't even be in this function in that case.
+			// A trailing abstemious operator would have already had its block
+			// removed.
+			while (tokens_[start_ - 1]._type ==
+				detail::token_type::OPENPAREN &&
+				tokens_[idx_ + 1]._type == detail::token_type::CLOSEPAREN)
+			{
+				--start_;
+				--iter_;
+				++idx_;
+			}
+
 			iter_ = tokens_.erase(iter_, tokens_.begin() + idx_ + 1);
 
-			if (iter_->_type == lexertl::detail::token_type::OR)
+			if (iter_->_type == detail::token_type::OR)
 				tokens_.erase(iter_);
-			else if (iter_->_type != lexertl::detail::token_type::BEGIN &&
-				(iter_ - 1)->_type == lexertl::detail::token_type::OR)
+			else if (iter_->_type != detail::token_type::BEGIN &&
+				(iter_ - 1)->_type == detail::token_type::OR)
 			{
 				tokens_.erase(--iter_);
 			}
